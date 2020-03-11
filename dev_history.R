706879a33fb05b3fa2c5c90d9015e974b2ca2bfd
Antoine Bichat <a.bichat@yahoo.fr>
2020-03-11 17:11:50 +0100

    First commit

---
+++ dev_history.R
@@ -0,0 +1,16 @@
+library(devtools)
+library(usethis)
+library(testthat)
+
+# use_build_ignore("dev_history.R")
+
+# use_gpl3_license("Antoine Bichat")
+
+# use_git()
+
+
+####
+
+use_tidy_description()
+
+check()
